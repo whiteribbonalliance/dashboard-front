1d4bae1d8217f8b62e0faf0d5816de93e684c636
Zairon Jacobs <zaijacobs@gmail.com>
2023-05-18 13:41:13 +0200

    edit header logos component

--- src/components/HeaderLogos/HeaderLogos.tsx
+++ src/components/HeaderLogos/HeaderLogos.tsx
@@ -12,7 +12,7 @@ export const HeaderLogos = ({ dashboard }: IHeaderLogosProps) => {
             <div>
                 <Link href={'/'}>
                     <Image
-                        className="max-h-[3rem] w-full max-w-[17rem] xl:max-h-[5rem]"
+                        className="max-h-[3rem] w-full max-w-[17rem] xl:max-h-[4rem]"
                         src="/wra_logo.png"
                         alt="white ribbon alliance logo"
                         width={1117}
@@ -74,7 +74,7 @@ export const HeaderLogos = ({ dashboard }: IHeaderLogosProps) => {
             <div>
                 <Link href={'/'}>
                     <Image
-                        className="my-[-17px] max-h-[5rem] w-full max-w-[20rem] xl:max-h-[5rem]"
+                        className="my-[-10px] max-h-[3.5rem] w-full max-w-[20rem] xl:max-h-[5rem]"
                         src="/whatyoungpeoplewant/pmnch_logo.png"
                         alt="pmnch logo"
                         width={1117}
@@ -88,7 +88,7 @@ export const HeaderLogos = ({ dashboard }: IHeaderLogosProps) => {
     switch (dashboard) {
         case Dashboards.WHAT_YOUNG_PEOPLE_WANT:
             return (
-                <div className="flex items-center gap-x-2.5">
+                <div className="flex items-center gap-x-5 xl:gap-x-3">
                     <WhiteRibbonAllianceLogo />
                     <_1point8Logo />
                     <PmnchLogo />
@@ -96,7 +96,7 @@ export const HeaderLogos = ({ dashboard }: IHeaderLogosProps) => {
             )
         case Dashboards.MIDWIVES_VOICES:
             return (
-                <div className="flex items-center gap-x-2.5">
+                <div className="flex items-center gap-x-5 xl:gap-x-3">
                     <WhatWomenWantLogo />
                     <PushWithWomenLogo />
                     <WhiteRibbonAllianceLogo />
@@ -104,7 +104,7 @@ export const HeaderLogos = ({ dashboard }: IHeaderLogosProps) => {
             )
         default:
             return (
-                <div className="flex items-center gap-x-2.5">
+                <div className="flex items-center gap-x-5 xl:gap-x-3">
                     <WhiteRibbonAllianceLogo />
                     <WhatWomenWantLogo />
                 </div>
