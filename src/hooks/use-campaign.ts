c0c54f453edb7eea84db9fdef4863bc9fbeeb165
Zairon Jacobs <zaijacobs@gmail.com>
2023-05-25 17:19:38 +0200

    edit regions

--- src/hooks/use-campaign.ts
+++ src/hooks/use-campaign.ts
@@ -8,12 +8,20 @@ import { useEffect } from 'react'
 export const useCampaignQuery = (dashboard: string) => {
     const filters = useFiltersStore((state: IFiltersState) => state.filters)
 
+    // Copy filters
+    const filter1 = { ...filters.filter1 }
+    const filter2 = { ...filters.filter2 }
+
+    // Remove alpha2 country code from region e.g. `ZW:Harare Province` -> `Harare Province`
+    filter1.regions = filter1.regions.map((region) => region.substring(3))
+    filter2.regions = filter2.regions.map((region) => region.substring(3))
+
     const campaignQuery = useQuery<ICampaign>({
         queryKey: ['campaign'],
         queryFn: () =>
             getCampaign(dashboard, {
-                filter_1: filters.filter1,
-                filter_2: filters.filter2,
+                filter_1: filter1,
+                filter_2: filter2,
             }),
         refetchOnWindowFocus: false,
         onSuccess: () => {},
