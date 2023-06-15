5ee851e5963b767bee31ddbe5821b1f461d194d0
Zairon Jacobs <zaijacobs@gmail.com>
2023-06-15 17:22:12 +0200

    edit middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,13 @@
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 import { dashboards, defaultLanguage, languages } from '@constants'
-import { isSubdomain } from '@utils'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl
     const hostname = request.headers.get('host') as string
-
-    // Get prod domains
     const prodDomains = (process.env.NEXT_PUBLIC_PROD_DOMAINS as string).split(' ')
-
-    // Get dev domain
     const devDomain = (process.env.NEXT_PUBLIC_DEV_DOMAIN as string) || '.localhost'
-
-    // Get the main subdomain
     const mainSubdomain = process.env.NEXT_PUBLIC_MAIN_SUBDOMAIN as string
 
     // Check if there is any supported language in the pathname or not
@@ -22,7 +15,7 @@ export function middleware(request: NextRequest) {
         (language) => !pathname.startsWith(`/${language.code}/`) && pathname !== `/${language.code}`
     )
 
-    // Redirect if there is no language
+    // Redirect if there is no language in pathname
     if (pathnameIsMissingLanguage) {
         // e.g. incoming request is /products
         // The new URL is now /en/products
@@ -30,7 +23,7 @@ export function middleware(request: NextRequest) {
     }
 
     // Get the custom domain/subdomain value by removing the root URL
-    // e.g. from 'whatwomenwant.whiteribbonalliance.org' remove '.whiteribbonalliance.org' to get 'whatwomenwant' (currentHost)
+    // e.g. from 'whatwomenwant.whiteribbonalliance.org' remove '.whiteribbonalliance.org' to get 'whatwomenwant'
     let currentHost: string | undefined
     if (process.env.NODE_ENV === 'production') {
         // Find prod domain
@@ -50,38 +43,31 @@ export function middleware(request: NextRequest) {
         currentHost = hostname?.replace(`${devDomain}:3000`, '')
     }
 
-    // If subdomain equals e.g. 'explore' and
-    // if a path is requested with a dashboard name, then ignore subdomain routing and use path instead
-    if (currentHost == mainSubdomain) {
+    // If subdomain equals the main subdomain and a path is requested that is a dashboard name, then ignore
+    // subdomain routing and use path instead
+    if (currentHost === mainSubdomain) {
         if (dashboards.some((dashboard) => pathname.endsWith(dashboard))) {
-            if (pathnameIsMissingLanguage) {
-                return NextResponse.redirect(new URL(`/${defaultLanguage.code}/${pathname}`, request.url))
-            } else {
-                // Prevent security issues
-                if (pathname.startsWith(`/dashboards_with_subdomain`)) {
-                    return new Response('404', { status: 404 })
-                }
-
-                // Get url
-                const nextUrl = request.nextUrl
-                nextUrl.pathname = `/dashboards_with_path${nextUrl.pathname}`
-
-                // Rewrite to the current hostname under the app/dashboards_with_path folder
-                return NextResponse.rewrite(nextUrl)
+            // Prevent security issues
+            if (pathname.startsWith(`/dashboards_with_path`)) {
+                return new Response('404', { status: 404 })
             }
+
+            const nextUrl = request.nextUrl
+            nextUrl.pathname = `/dashboards_with_path${nextUrl.pathname}`
+
+            // Rewrite to the current hostname under the app/dashboards_with_path folder
+            return NextResponse.rewrite(nextUrl)
         } else {
             return new Response('404', { status: 404 })
         }
     }
 
     // Prevent security issues – users should not be able to canonically access
-    // the app/dashboards_with_subdomain folder and its respective contents. This can also be done
-    // via rewrites to a custom 404 page
+    // the app/dashboards_with_subdomain folder and its respective contents
     if (pathname.startsWith(`/dashboards_with_subdomain`)) {
         return new Response('404', { status: 404 })
     }
 
-    // Get url
     const nextUrl = request.nextUrl
     nextUrl.pathname = `/dashboards_with_subdomain/${currentHost}${nextUrl.pathname}`
 
