3d6d1afb576bdea3dc7f4ef9042cfc5b06e5f694
Zairon Jacobs <zaijacobs@gmail.com>
2023-09-26 07:46:00 +0200

    remove second question from healthwellbeing

--- src/configurations/healthwellbeing.ts
+++ src/configurations/healthwellbeing.ts
@@ -13,7 +13,7 @@ export const configuration: IConfiguration = {
     respondentsNounSingular: 'woman',
     respondentsNounPlural: 'women',
     showVideoLink: 'https://www.youtube.com/watch?v=nBzide5J3Hk',
-    questionsAskedCodes: ['q1', 'q2'],
+    questionsAskedCodes: ['q1'],
     link: {
         id: DashboardName.HEALTHWELLBEING,
         title: title,
