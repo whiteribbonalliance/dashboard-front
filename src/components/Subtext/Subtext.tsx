f36f11d6395ad67819a5809dc5a1c63c7c4b0212
Zairon Jacobs <zaijacobs@gmail.com>
2023-10-03 09:52:59 +0200

    set sub text el using useeffect

--- src/components/Subtext/Subtext.tsx
+++ src/components/Subtext/Subtext.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from '@app/i18n/client'
 import { applyToThousandsSepOnText, classNames, getDashboardConfig } from '@utils'
 import { DashboardName } from '@enums'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const HtmlToReact = require('html-to-react')
 const HtmlToReactParser = require('html-to-react').Parser
@@ -18,6 +18,7 @@ interface ISubtextProps {
 export const Subtext = ({ dashboard, lang }: ISubtextProps) => {
     const { t } = useTranslation(lang)
     const config = getDashboardConfig(dashboard)
+    const [subtextElement, setSubtextElement] = useState<React.JSX.Element>()
 
     // Set link classes
     let linkClasses: string
@@ -30,67 +31,70 @@ export const Subtext = ({ dashboard, lang }: ISubtextProps) => {
     }
 
     // Set subtext element
-    let subtextElement: React.JSX.Element
-    switch (dashboard) {
-        case DashboardName.ECONOMIC_EMPOWERMENT_MEXICO:
-            subtextElement = (
-                <>
-                    <p className="mb-3">{applyToThousandsSepOnText(t('giz-asked-questions'), lang)}:</p>
-                    <p className="mb-3">
-                        “¿Qué es lo que más deseas o necesitas para encontrar empleo o un mejor empleo? Por favor,
-                        comparte sólo la petición más importante para ti.“ ({t(`${dashboard}-q1`)})
-                    </p>
-                    <p>
-                        “¿Qué debería hacer el gobierno para ayudarte a conseguir el trabajo de tus sueños? Por favor,
-                        comparte sólo la petición más importante para ti.“ ({t(`${dashboard}-q2`)})
-                    </p>
-                </>
-            )
-            break
-        case DashboardName.WHAT_YOUNG_PEOPLE_WANT:
-            let subtext = applyToThousandsSepOnText(t(`${config.campaignCode}-subtext`), lang)
+    useEffect(() => {
+        switch (dashboard) {
+            case DashboardName.ECONOMIC_EMPOWERMENT_MEXICO:
+                setSubtextElement(
+                    <>
+                        <p className="mb-3">{applyToThousandsSepOnText(t('giz-asked-questions'), lang)}:</p>
+                        <p className="mb-3">
+                            “¿Qué es lo que más deseas o necesitas para encontrar empleo o un mejor empleo? Por favor,
+                            comparte sólo la petición más importante para ti.“ ({t(`${dashboard}-q1`)})
+                        </p>
+                        <p>
+                            “¿Qué debería hacer el gobierno para ayudarte a conseguir el trabajo de tus sueños? Por
+                            favor, comparte sólo la petición más importante para ti.“ ({t(`${dashboard}-q2`)})
+                        </p>
+                    </>
+                )
+                break
+            case DashboardName.WHAT_YOUNG_PEOPLE_WANT:
+                let subtext = applyToThousandsSepOnText(t(`${config.campaignCode}-subtext`), lang)
 
-            // Set the link inside an anchor tag
-            subtext = subtext.replace(
-                'http://www.1point8.org',
-                '<a href="http://www.1point8.org">http://www.1point8.org</a>'
-            )
+                // Set the link inside an anchor tag
+                subtext = subtext.replace(
+                    'http://www.1point8.org',
+                    '<a href="http://www.1point8.org">http://www.1point8.org</a>'
+                )
 
-            // Processing instructions for the parser
-            const processingInstructions = [
-                {
-                    shouldProcessNode: (node: any) => node.name === 'a',
-                    processNode: (node: any, children: any, index: number) => {
-                        return (
-                            <Link
-                                key={index}
-                                href={node.attribs.href}
-                                className={classNames('underline', linkClasses)}
-                                target="_blank"
-                            >
-                                {children[0]}
-                            </Link>
-                        )
+                // Processing instructions for the parser
+                const processingInstructions = [
+                    {
+                        shouldProcessNode: (node: any) => node.name === 'a',
+                        processNode: (node: any, children: any, index: number) => {
+                            return (
+                                <Link
+                                    key={index}
+                                    href={node.attribs.href}
+                                    className={classNames('underline', linkClasses)}
+                                    target="_blank"
+                                >
+                                    {children[0]}
+                                </Link>
+                            )
+                        },
                     },
-                },
-                {
-                    shouldProcessNode: () => true,
-                    processNode: new HtmlToReact.ProcessNodeDefinitions().processDefaultNode,
-                },
-            ]
+                    {
+                        shouldProcessNode: () => true,
+                        processNode: new HtmlToReact.ProcessNodeDefinitions().processDefaultNode,
+                    },
+                ]
 
-            // Parse
-            subtextElement = new HtmlToReactParser().parseWithInstructions(subtext, () => true, processingInstructions)
-            break
-        case DashboardName.WOMENS_ECONOMIC_EMPOWERMENT:
-            subtextElement = <></>
-            break
-        case DashboardName.ALL_CAMPAIGNS:
-            subtextElement = <></>
-            break
-        default:
-            subtextElement = <p>{applyToThousandsSepOnText(t(`${config.campaignCode}-subtext`), lang)}</p>
-    }
+                // Parse
+                setSubtextElement(
+                    new HtmlToReactParser().parseWithInstructions(subtext, () => true, processingInstructions)
+                )
+                break
+            case DashboardName.WOMENS_ECONOMIC_EMPOWERMENT:
+                setSubtextElement(<></>)
+                break
+            case DashboardName.ALL_CAMPAIGNS:
+                setSubtextElement(<></>)
+                break
+            default:
+                setSubtextElement(<p>{applyToThousandsSepOnText(t(`${config.campaignCode}-subtext`), lang)}</p>)
+        }
+    }, [dashboard, lang, t, config, linkClasses])
 
-    return <div className="max-w-6xl text-center text-lg">{subtextElement}</div>
+    return <div className="max-w-6xl text-center text-lg">{subtextElement && subtextElement}</div>
 }
