f8f33a70f2613b06eea0670fa4fceab7bd4e941e
Zairon Jacobs <zaijacobs@gmail.com>
2024-02-07 09:11:49 +0100

    remove living setting from data-exchange

--- src/components/GraphsWrapper/GraphsWrapper.tsx
+++ src/components/GraphsWrapper/GraphsWrapper.tsx
@@ -68,7 +68,6 @@ export const GraphsWrapper = () => {
                     <ResponsesSampleTable />
                     <WorldBubbleMap />
                     <GenderBreakdownGraph />
-                    <LivingSettingsBreakdownGraph />
                     <HistogramGraph />
                 </>
             )
